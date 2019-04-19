e2cc45012de32a59105a6da7df6dcaf2a3d33bbc
bitPimps <bitpimps@gmail.com>
2019-04-19 09:53:50 -0500

    Initial import
    Importing assets and home page mockup

---
+++ wwwRoot/index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Children Are Unique Academy</title>
+		<meta name="description" content="">
+		<?php include_once("inc/meta-data.php"); ?>
+	</head>
+
+	<body>
+		<?php include_once("inc/nav-main.php"); DrawNavMain("Home"); ?>
+		<header class="hero">
+			<div class="container">
+				<h1>Children Are Unique Academy</h1>
+				<div class="sub-heading">
+					<p>Where Children Are #1</p>
+				</div>
+			</div>
+		</header>
+		<main role="main">
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Our Philosophy</h1>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et elit eleifend, venenatis lorem ac, rutrum mauris.</p>
+						<p><strong>Duis ut ante vitae sapien fringilla scelerisque.</strong><br>
+							Donec lectus nisi, convallis at tristique ac, blandit elementum nibh. Morbi nec eros quam. Nullam at rhoncus diam. Maecenas feugiat luctus dolor non tincidunt. Ut facilisis elit id aliquam interdum. Donec pulvinar ac lacus in tempus. Integer feugiat, leo non iaculis sodales, nisl enim cursus mi, eu volutpat ipsum turpis eu nisl. Duis ornare orci quis massa mollis, in convallis leo fringilla. Morbi vitae erat ut quam suscipit bibendum nec id diam.</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="https://via.placeholder.com/300">
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-4">
+						<img class="img-fluid" src="https://via.placeholder.com/300">
+					</div>
+					<div class="col-md-8">
+						<h1>Our Mission</h1>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et elit eleifend, venenatis lorem ac, rutrum mauris.</p>
+						<p><strong>Duis ut ante vitae sapien fringilla scelerisque.</strong><br>
+							Donec lectus nisi, convallis at tristique ac, blandit elementum nibh. Morbi nec eros quam. Nullam at rhoncus diam. Maecenas feugiat luctus dolor non tincidunt. Ut facilisis elit id aliquam interdum. Donec pulvinar ac lacus in tempus. Integer feugiat, leo non iaculis sodales, nisl enim cursus mi, eu volutpat ipsum turpis eu nisl. Duis ornare orci quis massa mollis, in convallis leo fringilla. Morbi vitae erat ut quam suscipit bibendum nec id diam.</p>
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Our Vision</h1>
+						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et elit eleifend, venenatis lorem ac, rutrum mauris.</p>
+						<p><strong>Duis ut ante vitae sapien fringilla scelerisque.</strong><br>
+							Donec lectus nisi, convallis at tristique ac, blandit elementum nibh. Morbi nec eros quam. Nullam at rhoncus diam. Maecenas feugiat luctus dolor non tincidunt. Ut facilisis elit id aliquam interdum. Donec pulvinar ac lacus in tempus. Integer feugiat, leo non iaculis sodales, nisl enim cursus mi, eu volutpat ipsum turpis eu nisl. Duis ornare orci quis massa mollis, in convallis leo fringilla. Morbi vitae erat ut quam suscipit bibendum nec id diam.</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="https://via.placeholder.com/300">
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x red"></i>
+							<i class="fa fa-university fa-stack-1x white"></i>
+						</span>
+						<h1 class="red">Qualified Teachers</h1>
+						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x yellow"></i>
+							<i class="fa fa-apple fa-stack-1x white"></i>
+						</span>
+						<h1 class="yellow">Nutritious Snacks</h1>
+						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x green"></i>
+							<i class="fa fa-child fa-stack-1x white"></i>
+						</span>
+						<h1 class="green">Enrichment Programs</h1>
+						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<span class="pb-5 fa-stack fa-5x">
+							<i class="fa fa-circle fa-stack-2x blue"></i>
+							<i class="fa fa-smile-o fa-stack-1x white"></i>
+						</span>
+						<h1 class="blue">Facility Features</h1>
+						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-6">
+						<img class="img-fluid mx-auto d-block" src="https://via.placeholder.com/300">
+						<h1 class="my-5 text-center">Program</h1>
+						<p>Curabitur nec est egestas, fermentum leo vel, aliquam ligula. Phasellus tempus dapibus elit id feugiat. Nulla mollis urna eu convallis fermentum. Suspendisse id mollis ex. Cras mattis justo id scelerisque volutpat. Cras pellentesque egestas nibh, sed feugiat urna venenatis at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="col-md-6">
+						<img class="img-fluid mx-auto d-block" src="https://via.placeholder.com/300">
+						<h1 class="my-5 text-center">A Brief History</h1>
+						<p>Curabitur nec est egestas, fermentum leo vel, aliquam ligula. Phasellus tempus dapibus elit id feugiat. Nulla mollis urna eu convallis fermentum. Suspendisse id mollis ex. Cras mattis justo id scelerisque volutpat. Cras pellentesque egestas nibh, sed feugiat urna venenatis at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+				</div>
+			</section>
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-12">
+						<img class="mx-auto d-block w-50" src="/caua/img/logos/children-are-unique-academy.jpg" alt="Children Are Unique Academy">
+					</div>
+				</div>
+			</section>
+		</main>
+		<?php include_once("inc/footer.php"); ?>
+	</body>
+</html>
