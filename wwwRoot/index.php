6af26122c81241832066710998ff1d5addc3330a
bitPimps <bitpimps@gmail.com>
2019-05-17 16:03:02 -0500

    Content updates
    Images and content

--- wwwRoot/index.php
+++ wwwRoot/index.php
@@ -23,25 +23,25 @@
 				<div class="row">
 					<div class="col-md-8">
 						<h1>Our Philosophy</h1>
-						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et elit eleifend, venenatis lorem ac, rutrum mauris.</p>
-						<p><strong>Duis ut ante vitae sapien fringilla scelerisque.</strong><br>
-							Donec lectus nisi, convallis at tristique ac, blandit elementum nibh. Morbi nec eros quam. Nullam at rhoncus diam. Maecenas feugiat luctus dolor non tincidunt. Ut facilisis elit id aliquam interdum. Donec pulvinar ac lacus in tempus. Integer feugiat, leo non iaculis sodales, nisl enim cursus mi, eu volutpat ipsum turpis eu nisl. Duis ornare orci quis massa mollis, in convallis leo fringilla. Morbi vitae erat ut quam suscipit bibendum nec id diam.</p>
+						<p class="lead">At Children Are Unique Academy, we believe in creating positive developmental interactions between children and their surroundings.</p>
+						<p><strong>The early years of childhood are the most influential in a child’s life.</strong><br>
+							We believe that young children learn most effectively when they have opportunities to explore their surroundings and experience a variety of play. We offer a nurturing, dynamic and diverse environment that encourages a love of learning and connection with others.</p>
 					</div>
 					<div class="col-md-4">
-						<img class="img-fluid" src="https://via.placeholder.com/300">
+						<img class="img-fluid" src="img/pages/philosophy.jpg" alt="Our Philosophy">
 					</div>
 				</div>
 			</section>
 			<section class="container py-5">
 				<div class="row">
 					<div class="col-md-4">
-						<img class="img-fluid" src="https://via.placeholder.com/300">
+						<img class="img-fluid" src="img/pages/mission.jpg" alt="Our Mission">
 					</div>
 					<div class="col-md-8">
 						<h1>Our Mission</h1>
-						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et elit eleifend, venenatis lorem ac, rutrum mauris.</p>
-						<p><strong>Duis ut ante vitae sapien fringilla scelerisque.</strong><br>
-							Donec lectus nisi, convallis at tristique ac, blandit elementum nibh. Morbi nec eros quam. Nullam at rhoncus diam. Maecenas feugiat luctus dolor non tincidunt. Ut facilisis elit id aliquam interdum. Donec pulvinar ac lacus in tempus. Integer feugiat, leo non iaculis sodales, nisl enim cursus mi, eu volutpat ipsum turpis eu nisl. Duis ornare orci quis massa mollis, in convallis leo fringilla. Morbi vitae erat ut quam suscipit bibendum nec id diam.</p>
+						<p class="lead">Children Are Unique Academy gently encourages children to discover their individual strengths and unique talents.</p>
+						<p><strong>Each child possesses a marvelous capacity to learn.</strong><br>
+							Not only to we provide quality care, but we also go the extra mile to help every individual child reach his or her full potential. Our teachers and caregivers lead by example, demonstrating that learning is best accomplished in spaces that are emotionally secure, physically safe, and all-around fun!</p>
 					</div>
 				</div>
 			</section>
@@ -49,12 +49,12 @@
 				<div class="row">
 					<div class="col-md-8">
 						<h1>Our Vision</h1>
-						<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et elit eleifend, venenatis lorem ac, rutrum mauris.</p>
-						<p><strong>Duis ut ante vitae sapien fringilla scelerisque.</strong><br>
-							Donec lectus nisi, convallis at tristique ac, blandit elementum nibh. Morbi nec eros quam. Nullam at rhoncus diam. Maecenas feugiat luctus dolor non tincidunt. Ut facilisis elit id aliquam interdum. Donec pulvinar ac lacus in tempus. Integer feugiat, leo non iaculis sodales, nisl enim cursus mi, eu volutpat ipsum turpis eu nisl. Duis ornare orci quis massa mollis, in convallis leo fringilla. Morbi vitae erat ut quam suscipit bibendum nec id diam.</p>
+						<p class="lead">Children Are Unique Academy strives to be a community where caregivers and families work together to nurture happy, healthy, and bright children.</p>
+						<p><strong>We build trust with the families we support.</strong><br>
+							We aim to create an environment where families feel confident about their child’s learning and development. By working in partnership with one another, we are able to give all children a sense of dignity, respect, and most of all, community.</p>
 					</div>
 					<div class="col-md-4">
-						<img class="img-fluid" src="https://via.placeholder.com/300">
+						<img class="img-fluid" src="img/pages/vision.jpg" alt="Our Vision">
 					</div>
 				</div>
 			</section>
@@ -66,23 +66,23 @@
 							<i class="fa fa-university fa-stack-1x white"></i>
 						</span>
 						<h1 class="red">Qualified Teachers</h1>
-						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+						<p>Our teachers are compassionate, trained caregivers.</p>
 					</div>
 					<div class="col-md-3 text-center">
 						<span class="pb-5 fa-stack fa-5x">
 							<i class="fa fa-circle fa-stack-2x yellow"></i>
 							<i class="fa fa-apple fa-stack-1x white"></i>
 						</span>
-						<h1 class="yellow">Nutritious Snacks</h1>
-						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+						<h1 class="yellow">Nutritious Meals</h1>
+						<p>We offer breakfast, lunch, and snacks throughout the day.</p>
 					</div>
 					<div class="col-md-3 text-center">
 						<span class="pb-5 fa-stack fa-5x">
 							<i class="fa fa-circle fa-stack-2x green"></i>
 							<i class="fa fa-child fa-stack-1x white"></i>
 						</span>
-						<h1 class="green">Enrichment Programs</h1>
-						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+						<h1 class="green">Flexible Scheduling</h1>
+						<p>We operate 6 AM-9 PM Sunday-Saturday, offering morning, daytime and evening care.</p>
 					</div>
 					<div class="col-md-3 text-center">
 						<span class="pb-5 fa-stack fa-5x">
@@ -90,21 +90,21 @@
 							<i class="fa fa-smile-o fa-stack-1x white"></i>
 						</span>
 						<h1 class="blue">Facility Features</h1>
-						<p>Sed at pellentesque libero. Proin maximus arcu nec consequat fermentum.</p>
+						<p>We have two buildings and an outdoor play area for children ages 6 weeks to 12 years old.</p>
 					</div>
 				</div>
 			</section>
 			<section class="container py-5">
 				<div class="row">
 					<div class="col-md-6">
-						<img class="img-fluid mx-auto d-block" src="https://via.placeholder.com/300">
+						<img class="img-fluid mx-auto d-block" src="img/pages/program.jpg" alt="Program">
 						<h1 class="my-5 text-center">Program</h1>
-						<p>Curabitur nec est egestas, fermentum leo vel, aliquam ligula. Phasellus tempus dapibus elit id feugiat. Nulla mollis urna eu convallis fermentum. Suspendisse id mollis ex. Cras mattis justo id scelerisque volutpat. Cras pellentesque egestas nibh, sed feugiat urna venenatis at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<p>Open Sunday-Saturday from 6 AM – 9 PM, we offer all-day care for children who are 6 weeks old to preschool-aged. We have one building for infants and toddlers, and a larger building for preschoolers. We also offer before and after-school care for elementary and middle school children (ages 6-12) in our larger building. We consult with families to create a program that makes sense for them and their busy schedules.</p>
 					</div>
 					<div class="col-md-6">
-						<img class="img-fluid mx-auto d-block" src="https://via.placeholder.com/300">
+						<img class="img-fluid mx-auto d-block" src="img/pages/history.jpg" alt="A Brief History">
 						<h1 class="my-5 text-center">A Brief History</h1>
-						<p>Curabitur nec est egestas, fermentum leo vel, aliquam ligula. Phasellus tempus dapibus elit id feugiat. Nulla mollis urna eu convallis fermentum. Suspendisse id mollis ex. Cras mattis justo id scelerisque volutpat. Cras pellentesque egestas nibh, sed feugiat urna venenatis at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<p>Children Are Unique Academy was founded in 2017 by Annette Smoote and Sherri Cole, who believe that all children are unique no matter their race, ethnicity or gender. Both women had over 24 years of experience working with children, and so they used their combined skills to form and operate their own childcare center. Annette and Sherri wanted to create a place where parents knew their children would be safe and well taken care of. Today, Annette and Sherri are fully committed to running a top-quality daycare center and go above and beyond to make sure everything runs smoothly.</p>
 					</div>
 				</div>
 			</section>
