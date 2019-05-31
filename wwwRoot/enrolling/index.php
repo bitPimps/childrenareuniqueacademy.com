5a88a6352e36caa58f3d35ca117849982053e440
bitPimps <bitpimps@gmail.com>
2019-05-31 15:32:40 -0500

    Enrolling page

---
+++ wwwRoot/enrolling/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Enrolling</title>
+		<meta name="description" content="">
+		<?php include_once("../inc/meta-data.php"); ?>
+	</head>
+
+	<body>
+		<?php include_once("../inc/nav-main.php"); DrawNavMain("Enrolling"); ?>
+		<main role="main">
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Enrolling</h1>
+						<p>To begin the enrollment process, call us at 314-300-8481 to schedule a tour of Children are Unique Academy. Together, we will develop a plan that makes the most sense for you and your child.</p>
+						<p>We invite parents and guardians to enroll their children ranging in age six weeks through twelve years to apply for full- or part-time care. They will be considered without regard to race, color, national origin, gender, age, religion, disability, or any other basis protected by federal, state, or local law.</p>
+						<p>Below are mandatory documents needed in the enrollment process. We encourage you to fill them out as soon as possible.</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="../img/pages/enrolling.jpg" alt="Enrolling">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+					</div>
+				</div>
+			</section>
+		</main>
+		<?php include_once("../inc/footer.php"); ?>
+	</body>
+</html>
