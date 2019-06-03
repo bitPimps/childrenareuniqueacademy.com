f6f08077da372069cf7671a5f9eef60d3e024aac
bitPimps <bitpimps@gmail.com>
2019-06-03 10:42:40 -0500

    Enrolling
    Adding Enrolling page

--- wwwRoot/enrolling/index.php
+++ wwwRoot/enrolling/index.php
@@ -18,6 +18,36 @@
 						<p>To begin the enrollment process, call us at 314-300-8481 to schedule a tour of Children are Unique Academy. Together, we will develop a plan that makes the most sense for you and your child.</p>
 						<p>We invite parents and guardians to enroll their children ranging in age six weeks through twelve years to apply for full- or part-time care. They will be considered without regard to race, color, national origin, gender, age, religion, disability, or any other basis protected by federal, state, or local law.</p>
 						<p>Below are mandatory documents needed in the enrollment process. We encourage you to fill them out as soon as possible.</p>
+						<div class="list-group">
+							<a href="../docs/Child-Care-Enrollment-Form.pdf" target="_blank" class="list-group-item list-group-item-action">
+								<div class="d-flex w-100 justify-content-between">
+									<h5 class="mb-1">For all children</h5>
+								</div>
+								<p class="mb-1">Childcare Enrollment Form</p>
+								<small class="text-muted">PDF</small>
+							</a>
+							<a href="../docs/Infant-and-Toddler-Feeding-and-Care-Plan.pdf" target="_blank" class="list-group-item list-group-item-action">
+								<div class="d-flex w-100 justify-content-between">
+									<h5 class="mb-1">For Infants/Toddlers</h5>
+								</div>
+								<p class="mb-1">Infant &amp; toddler Feeding and Care Plan</p>
+								<small class="text-muted">PDF</small>
+							</a>
+							<a href="../docs/Child-Medical-Examination-Report.pdf" target="_blank" class="list-group-item list-group-item-action">
+								<div class="d-flex w-100 justify-content-between">
+									<h5 class="mb-1">For Infants/Toddlers/Preschoolers</h5>
+								</div>
+								<p class="mb-1">Child Medical Examination Report</p>
+								<small class="text-muted">PDF</small>
+							</a>
+							<a href="../docs/Parents-Health-Statement-for-School-Age-Child.pdf" target="_blank" class="list-group-item list-group-item-action">
+								<div class="d-flex w-100 justify-content-between">
+									<h5 class="mb-1">For School-Age Children</h5>
+								</div>
+								<p class="mb-1">Parents Health Statement for School-Age Child</p>
+								<small class="text-muted">PDF</small>
+							</a>
+						</div>
 					</div>
 					<div class="col-md-4">
 						<img class="img-fluid" src="../img/pages/enrolling.jpg" alt="Enrolling">
--- wwwRoot/inc/nav-main.php
+++ wwwRoot/inc/nav-main.php
@@ -8,25 +8,25 @@
 				<div class="collapse navbar-collapse" id="navbarCCLLC">
 					<ul class="navbar-nav ml-auto">
 						<li class="nav-item<?php if($navMainOn=="Home"){?> active<?php } ?>">
-							<a class="nav-link" href="/index.php">Home<?php if($navMainOn=="Home"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/index.php">Home<?php if($navMainOn=="Home"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 						<li class="nav-item<?php if($navMainOn=="Classes"){?> active<?php } ?>">
-							<a class="nav-link" href="/training/index.php">Classes<?php if($navMainOn=="Classes"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/#">Classes<?php if($navMainOn=="Classes"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 						<li class="nav-item<?php if($navMainOn=="For Parents"){?> active<?php } ?>">
-							<a class="nav-link" href="/media/index.php">For Parents<?php if($navMainOn=="For Parents"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/#">For Parents<?php if($navMainOn=="For Parents"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 						<li class="nav-item<?php if($navMainOn=="Enrolling"){?> active<?php } ?>">
-							<a class="nav-link" href="/contact/index.php">Enrolling<?php if($navMainOn=="Enrolling"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/enrolling/index.php">Enrolling<?php if($navMainOn=="Enrolling"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 						<li class="nav-item<?php if($navMainOn=="Fundraising"){?> active<?php } ?>">
-							<a class="nav-link" href="/contact/index.php">Fundraising<?php if($navMainOn=="Fundraising"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/#">Fundraising<?php if($navMainOn=="Fundraising"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 						<li class="nav-item<?php if($navMainOn=="Contact"){?> active<?php } ?>">
-							<a class="nav-link" href="/contact/index.php">Contact<?php if($navMainOn=="Contact"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/#">Contact<?php if($navMainOn=="Contact"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 						<li class="nav-item<?php if($navMainOn=="About Us"){?> active<?php } ?>">
-							<a class="nav-link" href="/contact/index.php">About Us<?php if($navMainOn=="About Us"){?><span class="sr-only"> (current)</span><?php } ?></a>
+							<a class="nav-link" href="/caua/#">About Us<?php if($navMainOn=="About Us"){?><span class="sr-only"> (current)</span><?php } ?></a>
 						</li>
 					</ul>
 				</div>
