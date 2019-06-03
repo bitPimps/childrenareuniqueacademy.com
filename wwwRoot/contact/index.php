325d15fd19edad346c408ef5195868fc77fa7077
bitPimps <bitpimps@gmail.com>
2019-06-03 16:08:40 -0500

    Addming Contact page
    Adding Contact page and updating footer.

---
+++ wwwRoot/contact/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Contact</title>
+		<meta name="description" content="">
+		<?php include_once("../inc/meta-data.php"); ?>
+	</head>
+
+	<body>
+		<?php include_once("../inc/nav-main.php"); DrawNavMain("Contact"); ?>
+		<main role="main">
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Contact Us</h1>
+						<p>We would love to hear from you!</p>
+						<?php include_once("../inc/form-contact.php"); ?>
+					</div>
+					<div class="col-md-4">
+						<h3>Phone &amp; Email</h3>
+						<p>314-300-8481<br>
+							<a href="mailto:asmoote11@yahoo.com">asmoote11@yahoo.com</a></p>
+						<h3>Hours</h3>
+						<p>Sunday - Saturday<br>
+							6:00 am - 9:00 pm</p>
+						<h3>Postal Address</h3>
+						<p>3701 Cook Ave<br>
+							St. Louis, MO 63113</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+					</div>
+				</div>
+			</section>
+		</main>
+		<?php include_once("../inc/footer.php"); ?>
+	</body>
+</html>
