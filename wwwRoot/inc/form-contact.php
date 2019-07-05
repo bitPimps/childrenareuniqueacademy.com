10a759d760637d2f1a60d33f5772ac964d7a8791
bitPimps <bitpimps@gmail.com>
2019-07-05 11:07:49 -0500

    Going Live
    Pushing site live

--- wwwRoot/inc/form-contact.php
+++ wwwRoot/inc/form-contact.php
@@ -106,8 +106,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$mail->Password = "riscjxmdgxnteamr";
 			$mail->setFrom('webcontact@iconagility.com', 'Children Are Unique Academy Website');
 			$mail->addReplyTo('webcontact@iconagility.com', 'Children Are Unique Academy Website');
-			//$mail->addAddress('asmoote11@yahoo.com', 'Children Are Unique Academy');
-			$mail->addAddress('alyssa.hamilton@iconagility.com', 'Alyssa Hamilton');
+			$mail->addAddress('asmoote11@yahoo.com', 'Children Are Unique Academy');
+			//$mail->addAddress('alyssa.hamilton@iconagility.com', 'Alyssa Hamilton');
 			//$mail->addAddress('scott.lix@iconagility.com', 'Icon Website');
 			$mail->Subject = 'Children Are Unique Academy Website Contact';
 			$mail->Body =	"The user has sent an e-mail.\n" .
